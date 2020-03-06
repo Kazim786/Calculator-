fd6034d3a421ddaa11c37a5b4c6036f1d8946466
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-03-05 22:15:19 -0600

    use post module

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Calculator')
+    res.sendFile(__dirname + '/index.html')
 })
 app.get('/add/:numb1/:numb2', (req, res) => {
     var n1 = parseFloat(req.params.numb1);
     var n2 = parseFloat(req.params.numb2);
     let sum = n1 + n2
     res.send(`Total of ${n1} & ${n2} is ${sum}`)
+})
+
+app.post('/', (req, res) => {
+    
 })
 
 app.listen(3002, () => {
