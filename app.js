81bdf9feab26c10b22c9613bd178aaa10e823f53
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-03-05 23:38:46 -0600

    BMI calculator is done

--- app.js
+++ app.js
@@ -7,14 +7,32 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+app.get('/bmicalculator', (req, res) => {
+    res.sendFile(__dirname + '/bmiCalculator.html')
+})
 
+// app.post('/bmicalculator', (req, res) => {
+//     let n1 = Number(req.body.weight);
+//     let n2 = Number(req.body.height);
+//     result = n1 + n2 
+//     res.send(`Your BMI is ${result}`)
+
+// })
 
 app.post('/', (req, res) => {
 
-    n1 = Number(req.body.numb1)
-    n2 = Number(req.body.numb2)
+    let n1 = Number(req.body.numb1)
+    let n2 = Number(req.body.numb2)
     sum = n1 + n2
     res.send("The result of that calculation is " + sum)
+})
+
+app.post('/bmicalculator', (req, res) => {
+    let n1 = Number(req.body.weight);
+    let n2 = Number(req.body.height);
+    result = n1 + n2 
+    res.send(`Your BMI is ${result}`)
+
 })
 
 app.listen(3002, () => {
